31dcec3e2ab52365de8027778322b9a2b3147026
Flynn Letford <flynn.letford@rocos.io>
2020-03-06 13:32:44 +1300

    ROC-1172 - Updated output location for storing .go files converted from .xml.

--- commands/dialgen/gen.sh
+++ commands/dialgen/gen.sh
@@ -7,18 +7,16 @@ OUTFOLDER=$4
 
 
 pushd $INFOLDER > /dev/null
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ardupilotmega.xml > ${OUTFOLDER}/ardupilotmega.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ASLUAV.xml > ${OUTFOLDER}/ASLUAV.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go autoquad.xml > ${OUTFOLDER}/autoquad.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go common.xml > ${OUTFOLDER}/common.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go icarous.xml > ${OUTFOLDER}/icarous.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go matrixpilot.xml > ${OUTFOLDER}/matrixpilot.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go paparazzi.xml > ${OUTFOLDER}/paparazzi.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go python_array_test.xml > ${OUTFOLDER}/python_array_test.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go slugs.xml > ${OUTFOLDER}/slugs.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go standard.xml > ${OUTFOLDER}/standard.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go test.xml > ${OUTFOLDER}/test.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ualberta.xml > ${OUTFOLDER}/ualberta.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go uAvionix.xml > ${OUTFOLDER}/uAvionix.go
-go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ${FF}/mavlink_freightfish.xml > ${OUTFOLDER}/mavlink_freightfish.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ardupilotmega.xml > ${OUTFOLDER}/ardupilotmega/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ASLUAV.xml > ${OUTFOLDER}/ASLUAV/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go autoquad.xml > ${OUTFOLDER}/autoquad/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go common.xml > ${OUTFOLDER}/common/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go icarous.xml > ${OUTFOLDER}/icarous/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go matrixpilot.xml > ${OUTFOLDER}/matrixpilot/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go paparazzi.xml > ${OUTFOLDER}/paparazzi/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go slugs.xml > ${OUTFOLDER}/slugs/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go standard.xml > ${OUTFOLDER}/standard/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ualberta.xml > ${OUTFOLDER}/ualberta/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go uAvionix.xml > ${OUTFOLDER}/uAvionix/dialect.go
+go run ${MAINFOLDER}/main.go ${MAINFOLDER}/defdecoder.go ${FF}/mavlink_freightfish.xml > ${OUTFOLDER}/mavlink_freightfish/dialect.go
 popd > /dev/null
