41ee40e49fd6d6fddf578fda469600b641bb3dcd
Flynn Letford <flynn.letford@rocos.io>
2020-04-02 10:23:07 +1300

    ROC-1183 - Updated comments in endpoint_null.go.

--- endpoint_null.go
+++ endpoint_null.go
@@ -2,33 +2,37 @@ package gomavlib
 
 import "fmt"
 
-// this is an example struct that implements io.ReadWriteCloser.
+// NullEndpoint implements io.ReadWriteCloser.
 // it does not read anything and prints what it receives.
 // the only requirement is that Close() must release Read().
 type NullEndpoint struct {
 	readChan chan []byte
 }
 
+// NewNullEndpoint returns a pointer to a new NullEndpoint
 func NewNullEndpoint() *NullEndpoint {
 	return &NullEndpoint{
 		readChan: make(chan []byte),
 	}
 }
 
+// Close closes the channel c.readChan
 func (c *NullEndpoint) Close() error {
 	close(c.readChan)
 	return nil
 }
 
+// Read function of NullEndpoint
 func (c *NullEndpoint) Read(buf []byte) (int, error) {
 	read, ok := <-c.readChan
 	if ok == false {
-		return 0, fmt.Errorf("all right")
+		return 0, fmt.Errorf("failed to read from c.readChan")
 	}
 	n := copy(buf, read)
 	return n, nil
 }
 
+// Write function of NullEndpoint
 func (c *NullEndpoint) Write(buf []byte) (int, error) {
 	return len(buf), nil
 }
